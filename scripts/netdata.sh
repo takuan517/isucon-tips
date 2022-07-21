f7dca1c4cf1ae5f3bcf543c1d0cafbf49b968485
feles-ao42 <ao.42ao.42@gmail.com>
2022-07-21 17:48:13 +0900

    add change_ip_script

--- scripts/netdata.sh
+++ scripts/netdata.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt install netdata 
+sudo apt install netdata
+
+MYIP=$(hostname -i)
+MYIPV4=$(echo $MYIP | cut -d ' ' -f 1)
+sed "s/17.0.0.1/$MYIPV4/" -i /etc/netdata/netdata.conf
