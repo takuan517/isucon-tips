8138dbdffdc2a0f2f9260e33c24d12953f8004c7
boke0 <boke0@boke0ya.com>
2022-07-21 16:57:10 +0900

    update

--- scripts/alp.sh
+++ scripts/alp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_amd64.tar.gz
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_amd64.zip
 
 unzip alp_linux_amd64.zip
 
-sudo install ./alp /usr/local/bin
+sudo mv ./alp /usr/local/bin
